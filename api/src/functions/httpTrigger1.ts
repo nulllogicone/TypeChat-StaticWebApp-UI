5564029db2bd48849a5e8ce8ab4b071e64959c21
Frederic Luchting <github@luchting.de>
2023-10-17 11:19:14 +0200

    take post request with json and return json message

--- api/src/functions/httpTrigger1.ts
+++ api/src/functions/httpTrigger1.ts
@@ -3,9 +3,23 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function httpTrigger1(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    // const name = request.query.get('text') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}!` };
+    let prompt: string;
+
+    if (request.method === "POST") {
+        const requestBody = JSON.parse(await request.text());
+        prompt = requestBody.prompt || 'world';
+    } else {
+        prompt = request.query.get('prompt') || 'world';
+    }
+
+    return {
+        body: JSON.stringify({ message: `Hello, ${prompt}!` }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
 };
 
 app.http('httpTrigger1', {
