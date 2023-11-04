2556e698739b4fa6a1086a41aac2e57e28269d63
Frederic Luchting <github@luchting.de>
2023-11-05 00:42:35 +0100

    Add poems, nogoes and funfacts to resume

--- api/src/functions/resumeSchema.ts
+++ api/src/functions/resumeSchema.ts
@@ -1,11 +1,14 @@
 // The following is a schema definition of a resume
 
 export interface Resume {
-    title: string; // a headline of the resume
-    summary: string;  // An appealing summary of the candidate
+    title: string; // a trigger headline that makes the candidate stand out
+    summary: string;  // An appealing summary of the candidate with a professional touch
     experiences: Experience[];
     educations: Education[];
     skills: Skill[];
+    poem: string; // a poem of the condidate with a personal touch
+    nogoes: string[]; // a list of things the candidate does not want to do
+    funfacts: string[]; // a list of fun facts about the candidate
 }
 
 // work experience at a company with a given role and a short description of responsibilities you worked on, 
